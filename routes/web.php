47eac4867aef8ddc989c6a906b2858cbddb15132
pine <shenchaosong@yidaoit.com>
2019-12-18 18:19:17 +0800

    learn route

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,55 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/*Route::get('/', function () {
+    return view('welcome',['website'=>'pine']);
+});*/
+Route::get('/hello', function () {
+    return 'hello，pine！';
+});
+
+Route::match(['get', 'post'], 'foo', function () {
+    return 'This is a request from get or post';
+});
+
+Route::any('bar', function () {
+    return 'This is a request from any HTTP verb';
+});
+
+Route::view('/', 'welcome',['website'=>'pine']);
+
+/*Route::get('user/{id}', function ($id) {
+    return 'User ' . $id;
+});*/
+
+
+Route::get('posts/{id}/comments/{comment}', function ($postId, $commentId) {
+    return $postId . '-' . $commentId;
+});
+
+/*Route::get('user/profile', function () {
+    // 通过路由名称生成 URL
+    return 'my url: ' . route('profile');
+})->name('profile');*/
+
+/*Route::get('user/profile', 'UserController@showProfile')->name('profile');
+
+Route::get('redirect', function() {
+    // 通过路由名称进行重定向
+    return redirect()->route('profile');
+});*/
+
+Route::get('user/{id}/profile', function ($id) {
+    $url = route('profile', ['id' => $id]);
+    return $url;
+})->name('profile');
+
+Route::prefix('admin')->group(function () {
+    Route::get('users', function () {
+        // Matches The "/admin/users" URL
+        return 'User admin';
+    });
+});
+Route::get('users/{user}', function (App\User $user) {
+    return $user;
 });
